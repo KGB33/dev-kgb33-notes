0e181a60db5817f177753313858b17b80441f30b
KGB33 <github@bassingthwaite.org>
2020-09-25 14:17:22 -0600

    added bootstrap & index/base html

--- notes/__init__.py
+++ notes/__init__.py
@@ -2,11 +2,15 @@ import os
 
 from flask import Flask
 
+from flask_bootstrap import Bootstrap
 
 
 def create_app(config=os.environ["APP_CONFIG"]):
     app = Flask(__name__)
     app.config.from_object(config)
+
+    # Plugins
+    Bootstrap(app)
 
     from notes.main import bp as main_bp
     app.register_blueprint(main_bp, url_prefix='')
