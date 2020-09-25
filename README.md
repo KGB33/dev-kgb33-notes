5b6d709a893e1ff3540214e25595b006abf38140
KGB33 <github@bassingthwaite.org>
2020-09-25 14:01:47 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Goal:
+
+Create a (static) site that will act as a blog/note taking platform.
+Each endpoint will map to a static HTML document that contains the content.
+
+# Project Structure
+
+./
---
+++ notes/configs.py
@@ -0,0 +1,19 @@
+import os
+
+class Base:
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
+
+class Testing(Base):
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+class Development(Base):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class Production(Base):
+    DEBUG = False
+
