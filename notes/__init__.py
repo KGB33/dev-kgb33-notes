e63617d0f68a197cf57f6fe31f11c928d8cc2ab2
KGB33 <github@bassingthwaite.org>
2020-09-26 10:14:05 -0600

    added global BASE_DIR var

--- notes/__init__.py
+++ notes/__init__.py
@@ -1,8 +1,11 @@
 import os
+from pathlib import Path
 
 from flask import Flask
 
 from flask_bootstrap import Bootstrap
+
+BASE_DIR = Path(__file__).parent #os.path.dirname(os.path.realpath(__file__))
 
 
 def create_app(config=os.environ["APP_CONFIG"]):
