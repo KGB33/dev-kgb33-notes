6bb9e147d320d544fac2a02e604b83616b58d47a
KGB33 <github@bassingthwaite.org>
2020-10-23 09:45:16 -0600

    added custom error handling for 404

--- notes/__init__.py
+++ notes/__init__.py
@@ -12,11 +12,17 @@ def create_app(config=os.environ["APP_CONFIG"]):
     app = Flask(__name__)
     app.config.from_object(config)
 
-    # Plugins
+    # --------- Register Plugins ---------
     Bootstrap(app)
 
+    # --------- Register Blueprints ---------
     from notes.main import bp as main_bp
 
     app.register_blueprint(main_bp, url_prefix="")
+
+    # --------- Register Errors ---------
+    from notes.errors import not_found
+
+    app.register_error_handler(404, not_found)
 
     return app
---
+++ notes/errors.py
@@ -0,0 +1,2 @@
+def not_found(e):
+    return "Page not found", 404
