4e41821294428134fa9b0f7a7447fc41fd22d291
KGB33 <github@bassingthwaite.org>
2020-10-23 09:44:49 -0600

    fixed dir/file names when not on personal laptop

--- notes/main/routes.py
+++ notes/main/routes.py
@@ -1,4 +1,8 @@
-from flask import render_template, request
+from pathlib import Path
+
+from flask import render_template, request, abort
+
+from notes import BASE_DIR
 
 from notes.main import bp
 
@@ -8,9 +12,18 @@ from notes.markdown import render_markdown, find_markdown_files
 @bp.route("/")
 @bp.route("/index")
 def index():
-    return render_template("index.html", files=find_markdown_files())
+    files = {p.name: f for p, f in find_markdown_files().items()}
+    return render_template("index.html", files=files)
 
 
 @bp.route("/doc")
 def document():
-    return render_markdown(f"{request.args['dir']}/{request.args['file']}")
+    path = Path(f"{BASE_DIR}/static/markdown/")
+    if request.args["dir"] != "markdown":
+        path = path / request.args["dir"]
+    path = path / request.args["file"]
+
+    if not path.exists():
+        abort(404)
+
+    return render_markdown(path)
